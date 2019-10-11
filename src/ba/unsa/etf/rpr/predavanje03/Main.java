3a6e27fb0941c3a62037eb6538f9444654415aee
NadzaM <nmemic2@etf.unsa.ba>
2019-10-11 17:58:58 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/predavanje03/Main.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.rpr.predavanje03;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Dobro došli na RPR");
+        // write your code here
+    }
+}
